2cbc7258137e4f8c7a26ec69d6d7177a421115eb
arenchilingaryan <58465410+arenchilingaryan@users.noreply.github.com>
2020-02-22 17:11:50 +0300

    Updates

---
+++ precache-manifest.21543921d519dd0b417e4e157b2aa077.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7b609af8f08feb114793ae8444fc8f",
+    "url": "/jsonplaceholder-api/index.html"
+  },
+  {
+    "revision": "bb295414d8e2a60c1c2e",
+    "url": "/jsonplaceholder-api/static/css/2.c6250e3a.chunk.css"
+  },
+  {
+    "revision": "2c578b6c6227adac2c2a",
+    "url": "/jsonplaceholder-api/static/css/main.be20950a.chunk.css"
+  },
+  {
+    "revision": "bb295414d8e2a60c1c2e",
+    "url": "/jsonplaceholder-api/static/js/2.ab6e7fbe.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/jsonplaceholder-api/static/js/2.ab6e7fbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c578b6c6227adac2c2a",
+    "url": "/jsonplaceholder-api/static/js/main.958695b5.chunk.js"
+  },
+  {
+    "revision": "99a71c66f03042ba2e32",
+    "url": "/jsonplaceholder-api/static/js/runtime-main.ac2b8e81.js"
+  }
+]);
